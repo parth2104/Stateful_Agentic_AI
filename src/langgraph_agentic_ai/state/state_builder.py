a898fa85455c0d100c414483f488d5d40d9a1965
parth2104 <barotparth512@gmail.com>
2026-02-10 19:42:05 +0530

    Add State class to represent the structure of the state in the graph

---
+++ src/langgraph_agentic_ai/state/state_builder.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel,Field
+from langchain_core.messages import BaseMessage 
+from langgraph.graph.message import add_messages 
+from typing import Annotated
+from typing_extensions import List 
+
+
+
+
+
+class State(BaseModel):
+    """
+    Represent the structure of the state in graph
+    """
+    messages: Annotated [List[BaseMessage],add_messages]
+
