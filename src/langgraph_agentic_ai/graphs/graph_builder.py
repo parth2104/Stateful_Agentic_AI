1c8ce0039d5b5a9df320b36996ac4f7b9001d866
parth2104 <barotparth512@gmail.com>
2026-02-11 18:52:07 +0530

    created the graph

--- src/langgraph_agentic_ai/graphs/graph_builder.py
+++ src/langgraph_agentic_ai/graphs/graph_builder.py
@@ -1,9 +1,6 @@
 from langgraph.graph  import StateGraph,START,END
 from src.langgraph_agentic_ai.state.state_builder import State 
-
-
-
-
+from src.langgraph_agentic_ai.nodes.chatbot_node import BasicChatbotNode
 
 
 
@@ -17,6 +14,8 @@ class GraphBuilder:
 
     def build_chatbot(self):
 
-        self.graph.add_node("chatbot","")
+        self.basic_chabot_bode=BasicChatbotNode(self.model)
+
+        self.graph.add_node("chatbot",self.basic_chabot_bode.process)
         self.graph.add_edge(START,"chatbot")
         self.graph.add_edge("chatbot",END)
