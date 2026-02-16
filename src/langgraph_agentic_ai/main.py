16f5aa839781eb529d882e8ea829900d9868e238
parth2104 <barotparth512@gmail.com>
2026-02-16 19:32:33 +0530

    implement LLM model initialization and graph setup in load_langgraph_agentic_ai function

--- src/langgraph_agentic_ai/main.py
+++ src/langgraph_agentic_ai/main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from src.langgraph_agentic_ai.ui.streamlit.load_ui import LoadStreamlitUI
+from src.langgraph_agentic_ai.LLMs.llm import LLM
+from src.langgraph_agentic_ai.graphs.graph_builder import GraphBuilder
 
 
 def load_langgraph_agentic_ai():
@@ -15,3 +17,25 @@ def load_langgraph_agentic_ai():
         return
 
     user_message = st.chat_input("Enter your message")
+
+    if user_message:
+        try:
+            llm_config=LLM(user_control_input=user_input)
+            model=llm_config.get_llm_model()
+
+            if not model:
+                st.error("LLM model could not be initialise")
+                return
+            use_case=user_input.get("usecase")
+
+            if not use_case:
+                st.error("use case is not selected")
+                return
+            
+            graph_builder=GraphBuilder(model)
+
+            graph=graph_builder.setup_graph(use_case)
+            
+
+        except Exception as e:
+            pass
